5144f3662f15b6aa01d9f1d44c9fd8410704abb1
Piotr Napierala <piotr.napierala94@gmail.com>
2022-03-21 16:19:27 +0100

    Add config files

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,18 @@
 module.exports = {
+  extends: ["@react-native-community", "plugin:react-hooks/recommended"],
+  rules: {
+    "@typescript-eslint/no-unused-vars": "error",
+    "sort-imports": [
+      "error",
+      {
+        ignoreCase: true,
+        ignoreMemberSort: true,
+        ignoreDeclarationSort: true,
+        allowSeparatedGroups: true,
+      },
+    ],
+  },
+
+  plugins: ["react", "react-native", "react-hooks", "import"],
   root: true,
-  extends: '@react-native-community',
 };
