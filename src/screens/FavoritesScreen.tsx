861258623610902f4c0f61cbc15041584ab640dc
Piotr Napierala <piotr.napierala94@gmail.com>
2022-04-13 17:52:39 +0200

    Add

--- src/screens/FavoritesScreen.tsx
+++ src/screens/FavoritesScreen.tsx
@@ -1,12 +1,23 @@
 import * as React from "react"
 import { StyleSheet, Text } from "react-native"
 
+import { appContext } from "contexts/contextApi"
 import { SafeAreaView } from "react-native-safe-area-context"
 
-export const FavoritesScreen = ({}) => {
+import { Typography, Colors } from "styles/index"
+
+export const FavoritesScreen = () => {
+  const { colorScheme } = appContext()
   return (
     <SafeAreaView style={styles.safeArea}>
-      <Text>Welcome from Favorites Screen</Text>
+      <Text
+        style={
+          colorScheme === "light"
+            ? styles.headerText_light
+            : styles.headerText_dark
+        }>
+        Welcome from Favorites Screen
+      </Text>
     </SafeAreaView>
   )
 }
@@ -14,5 +25,13 @@ export const FavoritesScreen = ({}) => {
 const styles = StyleSheet.create({
   safeArea: {
     flex: 1,
+  },
+  headerText_light: {
+    ...Typography.header.x40,
+    color: Colors.primary.s600,
+  },
+  headerText_dark: {
+    ...Typography.header.x40,
+    color: Colors.primary.neutral,
   },
 })
