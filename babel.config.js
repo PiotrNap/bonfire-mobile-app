860e6df66918a1f8e4cea1cdbd6b271256310802
Piotr Napierala <piotr.napierala94@gmail.com>
2022-04-17 20:08:36 +0200

    Add babel-plugin-transform-remove-console

--- babel.config.js
+++ babel.config.js
@@ -1,54 +1,59 @@
 module.exports = function (api) {
   api.cache(true)
 
-  return {
-    presets: ["module:metro-react-native-babel-preset"],
-    plugins: [
-      [
-        "module:react-native-dotenv",
-        {
-          moduleName: "@env",
-          allowUndefined: false,
-          safe: true,
-        },
-      ],
-      [
-        require.resolve("babel-plugin-module-resolver"),
-        {
-          root: ["./src/"],
-          alias: {
-            // define aliases to shorten the import paths
-            assets: "./src/assets",
-            common: "./src/common",
-            components: "./src/components",
-            containers: "./src/containers",
-            contexts: "./src/contexts",
-            lib: "./src/lib",
-            icons: "./src/assets/icons",
-            images: "./src/assets/images",
-            interfaces: "./src/common/interfaces",
-            Api: "./src/services/Api",
-            screens: "./src/screens",
-            stacks: "./src/stacks",
-            styles: "./src/styles",
-            tabs: "./src/tabs",
-            types: "./src/common/types",
-            utils: "./src/lib/utils.ts",
-          },
-          extensions: [
-            ".js",
-            ".jsx",
-            ".tsx",
-            ".ts",
-            ".ios.js",
-            ".android.js",
-            ".jpg",
-            ".png",
-          ],
-        },
-      ],
-      // reanimated has to be listed as last plugin.
-      "react-native-reanimated/plugin",
+  const presets = ["module:metro-react-native-babel-preset"]
+  const plugins = [
+    [
+      "module:react-native-dotenv",
+      {
+        moduleName: "@env",
+        allowUndefined: false,
+        safe: true,
+      },
     ],
+    [
+      require.resolve("babel-plugin-module-resolver"),
+      {
+        root: ["./src/"],
+        alias: {
+          // define aliases to shorten the import paths
+          assets: "./src/assets",
+          common: "./src/common",
+          components: "./src/components",
+          containers: "./src/containers",
+          contexts: "./src/contexts",
+          lib: "./src/lib",
+          icons: "./src/assets/icons",
+          images: "./src/assets/images",
+          interfaces: "./src/common/interfaces",
+          Api: "./src/services/Api",
+          screens: "./src/screens",
+          stacks: "./src/stacks",
+          styles: "./src/styles",
+          tabs: "./src/tabs",
+          types: "./src/common/types",
+          utils: "./src/lib/utils.ts",
+        },
+        extensions: [
+          ".js",
+          ".jsx",
+          ".tsx",
+          ".ts",
+          ".ios.js",
+          ".android.js",
+          ".jpg",
+          ".png",
+        ],
+      },
+    ],
+    // reanimated has to be listed as last plugin.
+    "react-native-reanimated/plugin",
+  ]
+
+  if (api.env("production")) plugins.push("transform-remove-console")
+
+  return {
+    presets,
+    plugins,
   }
 }
