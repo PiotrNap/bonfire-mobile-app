831e51692a311dd86a3f4fbd6db2aef63779d940
Piotr Napierala <piotr.napierala94@gmail.com>
2023-11-07 09:23:05 +0100

    remove unused words lists from bip39 build source

--- metro.config.js
+++ metro.config.js
@@ -1,4 +1,5 @@
 const { getDefaultConfig, mergeConfig } = require("@react-native/metro-config")
+const blacklist = require("metro-config/src/defaults/exclusionList")
 
 /**
  * Metro configuration
@@ -6,6 +7,12 @@ const { getDefaultConfig, mergeConfig } = require("@react-native/metro-config")
  *
  * @type {import('metro-config').MetroConfig}
  */
-const config = {}
+const config = {
+  resolver: {
+    blacklistRE: blacklist([
+      /node_modules\/bip39\/src\/wordlists\/(japanese|spanish|italian|french|korean|czech|portuguese|chinese_traditional|chinese_simplified)\.json$/,
+    ]),
+  },
+}
 
 module.exports = mergeConfig(getDefaultConfig(__dirname), config)
